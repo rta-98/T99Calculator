33e0ef896bffe9523c6d2d481d1e007ec21df80a
rta-98 <treyjroupp@gmail.com>
2025-12-16 10:00:54 -0700

    mol to image conversion

--- main.py
+++ main.py
@@ -1,104 +1,58 @@
-from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Form, Request, HTTPException
+from fastapi.responses import Response, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles 
-# from starlette.middleware.sessions import SessionMiddleware
-from pydantic import BaseModel, ConfigDict 
-from typing import Optional 
-import numpy as np
+from pydantic import BaseModel, ConfigDict, Field, field_validator 
+from typing import Optional, List  
+import io 
+import csv
 import os
 import rdkit
+import requests
 import glob
 import pandas as pd
-from rdkit import Chem
-from rdkit.Chem import PandasTools
-from rdkit import DataStructs
-from rdkit.Chem import rdchem
-from rdkit.Chem import Draw
+import numpy as np
+from rdkit import Chem, DataStructs
+from rdkit.Chem import rdchem, Draw, PandasTools
 import subprocess
 import re
 import pubchempy as pubpy
+from pathlib import Path
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image as pyxl_img
+from openpyxl.utils import get_column_letter as col_char
+from PIL import Image as pil_img
+from utility.services import InputValid, MolData 
 
+#|%%--%%| <tKnZ0ck33L|NLVW3NX2Sg>
 app = FastAPI()
 # app.add_middleware() 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates") 
 
-# class UserProfie(BaseModel):
-#     def __init__(self, user_id=None, is_auth=False):
-#         self.user_id = user_id
-#         self.is_auth = is_auth
-#     def save_result     
-
-#|%%--%%| <KnOGX2r6W3|DyftkKJNz8>
-class User(BaseModel):
-    user_id: Optional[str] = None 
-    smi_in: Optional[str] = None
-    smi_canon: Optional[str] = None
-    mol_in: Optional[rdchem.Mol] = None
-    mol_out: Optional[rdchem.Mol] = None
-
-    model_config = ConfigDict(arbitrary_types_allowed=True) 
-
-    class UserInput:
-        def __init__(self, smi_in: str):
-            self.smi_in = smi_in
-            self.user_id = user_id
-
-        def udir(self):
-            directory = f"user_{user_id}"
-            os.makedirs(directory, 
-        def veri_smi(self):
-            self.smi_in.strip() == "":
-            if not self.smi_in: 
-                print("Blank field")
-                return
-            try:
-                smi_mol  = Chem.CannonSmiles(self.smi_in)
-                return smi_mol
-            except Exception as e: 
-                print({f"Invalid SMILES: {e}"}) 
-                return None 
-        
-        def smi_2_mol(self):
-            verid_smi = self.veri_smi()
-            if verid_smi is None:
-                return None
-            else:
-                mol_obj = Chem.MolFromSmiles(verid_smi) 
-                if mol_obj is not None:
-                    return verid_smi, mol_obj
-                else: 
-                    return None
-
-        def mol_2_img(self):
-            verid_smi = self.veri_smi()
-
-            try:
-                smi_2_iupac = pubpy.get_compounds(self.smi_in, 'smiles')
-            except Exception as e:
-                return None:
-          
-           
-            
-            
-#|%%--%%| <DyftkKJNz8|f3uF9mS9gd>
-
-
-#|%%--%%| <f3uF9mS9gd|ngfGKgl8C5>
-obj = User.UserInput("test") 
-p1 = obj.veri_smi("test")
-print(p1)
-#|%%--%%| <ngfGKgl8C5|tKnZ0ck33L>
 # Serve "Home" as page 0 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request): 
     return templates.TemplateResponse(
         "index.html", {
-        "request": request
+        "request": request,
         "page": {0: "Home"}
     })
 
+@app.post("/canonical-smiles"):
+async def 
+
+@app.post("/smiles-structure")
+async def display_image(data: InputValid):
+    try:
+        image_bytes = MolData.img_2_bytes(data.smi_in) 
+        return Response(content=image_bytes, media_type='image/png')
+    except HTTPException:
+        raise # re-raise error so FastAPI can handle it
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'ERROR: {e}'}
+
+#|%%--%%| <NLVW3NX2Sg|SVBOFjN41o>
 @app.post("/input-smiles")
 async def grab_smi(
         request: Request,
@@ -112,7 +66,7 @@ async def grab_smi(
         
         molStruct = Chem.MolFromSmiles(
         smiles = Chem.MolToSmiles(       
-#|%%--%%| <tKnZ0ck33L|UJDrNroodF>
+#|%%--%%| <SVBOFjN41o|UJDrNroodF>
 
 
 
