ca6b6273ccbc9a878eb316dc611b1270b127ba77
rta-98 <treyjroupp@gmail.com>
2026-02-11 16:17:37 -0700

    Developing class to populate database

---
+++ utility/sorting.py
@@ -0,0 +1,80 @@
+from utility.services import * 
+from utility.display import *
+from utility.smiles import * 
+from rdkit import Chem
+from pathlib import Path 
+from itertools import zip_longest 
+#|%%--%%| <sbUXtDskGX|02x1QgN63r>
+# Only works in script file 
+# root_dir = Path(__file__).resolve().parent 
+
+# txt_path = root_dir / "../smi_pdb_data
+
+# --- Imported CLass Ref. ---
+#         self.dir = dir_path
+#         self.smiles_list = []
+#         self.mols_list = []
+#         self.smi_fname = []
+#         self.smiles_dud_list = []
+#         self.pdb_files = []
+
+base = Path.cwd() 
+data_path = base / "./smi_pdb_data"
+
+
+print(data_path)
+
+# for i in data_path.rglob('*.smi'):
+#     print(i)
+
+
+base = Path.cwd() 
+data_path = base / "./smi_pdb_data"
+
+class BytesPDB:
+
+    def __init__(self, data_path, fparse: SmileFileParser | None = None): 
+        self.data_path = data_path 
+        self.fparse = fparse or SmileFileParser(data_path) 
+        self.pdb_posix = []
+        self.pdb_bytes = []
+        self.pdb_IUPAC = []
+        self.pdb_smarts = []
+        self.pdb_smiles = [] 
+        self.smi_pdb_dict = {
+                "SMILES": [],
+                "SMARTS": [], 
+                "PDB Bytes": [],
+                "PDB Files": [],
+                "PDB Posix": [],
+                "PDB IUPAC": [],
+                "PDB Bytes": [],
+                "None Idxs": []
+        }
+
+    def bookeeper(self):
+        self.fparse = SmileFileParser(str(data_path)) 
+        self.fparse.smi_populate()
+        for i, (smiles, pdbs) in enumerate(zip_longest(self.fparse.smiles_list, self.fparse.pdb_files)):
+            self.smi_pdb_dict[i] = {
+                "SMILES": smiles,
+                "PDB Files": str(pdbs),
+                "PDB Posix": [pdbs],
+                "None Idxs": [
+                    i for i, (smiles, pdbs) in enumerate(zip_longest(self.fparse.smiles_list, self.fparse.pdb_files)) if smiles is None or pdbs is None
+                ]
+            }
+#            self.pdb_posix.append(pdbs) 
+
+    def correlator(self): 
+        pass 
+        
+
+    def pdbs_to_bytes(self): 
+        pass
+
+#|%%--%%| <02x1QgN63r|7wgqOXTKKq>
+zed = BytesPDB(data_path) 
+zed.bookeeper()
+zed.smi_pdb_dict 
+zed.pdb_posix
