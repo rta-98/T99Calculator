e7ebb3762283fb061869f284f75f1c007d830c5a
rta-98 <treyjroupp@gmail.com>
2026-02-12 14:52:24 -0700

    count_dihedral() working.

--- utility/sorting.py
+++ utility/sorting.py
@@ -31,23 +31,27 @@ class BytesPDB:
                 "PDB Bytes": [],
                 "IDX": []
         }
+##
+#    def mol_pdb(self):
+#        for pdb in self.fparse.pdb_files:
+#            try: 
+#                mol_pdb = Chem.MolFromPDBFile(pdb, sanitize=False, removeHs=False)
+#            except OSError: 
+#                mol_pdb = None 
 
-    def mol_pdb(self):
-        for pdb in self.fparse.pdb_files:
-            try: 
-                mol_pdb = Chem.MolFromPDBFile(pdb, sanitize=False, removeHs=False)
-            except OSError: 
-                mol_pdb = None 
-            self.smi_pdb_dict["PDB MOLS"].append(mol_pdb)  
-
+    
     def bookeeper(self) -> dict:
         self.fparse = SmileFileParser(str(data_path)) 
         self.fparse.smi_populate()
         for idx, (smiles, pdbs) in enumerate(zip_longest(self.fparse.smiles_list, self.fparse.pdb_files)):
+            try: 
+                mol_pdb = Chem.MolFromPDBFile(str(pdbs))
+            except OSError: 
+                mol_pdb = None 
+            self.smi_pdb_dict["PDB MOLS"].append(mol_pdb)  
             self.smi_pdb_dict["IDX"].append(idx) 
             self.smi_pdb_dict["SMILES"].append(smiles) 
             self.smi_pdb_dict["PDB Files"].append(str(pdbs)) 
-#            self.smi_pdb_dict["PDB MOLS"].append(pdbs) 
             self.smi_pdb_dict["PDB Posix"].append(pdbs) 
             self.smi_pdb_dict["IUPAC"].append(pdbs.stem) 
             self.smi_pdb_dict["MOLS"].append(Chem.MolFromSmiles(smiles)) 
@@ -63,17 +67,17 @@ class MoleculeSorter:
         self.mols = self.imported_mol_data["MOLS"]
         self.pdb_mols = self.imported_mol_data["PDB MOLS"]
         self.mol_sorted_dict: dict = {}
-
+#
     def analyze_all(self): 
-        for iupac, mol in zip(self.iupacs, self.mols):
-            self.mol_sorted_dict[iupac] = self.analyzer(mol) 
-        return self.mol_sorted_dict 
+       for iupac, mol, pdb_mol in zip(self.iupacs, self.mols, self.pdb_mols):
+           self.mol_sorted_dict[iupac] = self.analyzer(mol, pdb_mol) 
+       return self.mol_sorted_dict 
 
-    def analyzer(self, mol):
+    def analyzer(self, mol, pdb_mol):
         return {
             "Num. Atoms": self.count_atoms(mol),
             "Connectivity": self.count_motif(mol),
-            "Torsions": self.count_dihedrals(mol),
+            "Torsions": self.count_dihedral(pdb_mol),
         }
 
     def count_atoms(self, mol): 
@@ -133,19 +137,50 @@ class MoleculeSorter:
                 }
         return result 
 
-    def count_dihedrals(self, mol): 
-        pass 
+    def count_dihedral(self, pdb_mol=None):
+        ROT_BONDS = str('[!$(*#*)&!D1]-&!@[!$(*#*)&!D1]')
+        DUMMY_MOL = self.pdb_mols[2]
+        rot_bonds_smarts = Chem.MolFromSmarts(ROT_BONDS)
+        try: 
+            confs = pdb_mol.GetConformer() 
+            matches = pdb_mol.GetSubstructMatches(rot_bonds_smarts)
+            imp_mol = pdb_mol
+        except Exception: 
+            confs = DUMMY_MOL.GetConformer() 
+            matches = DUMMY_MOL.GetSubstructMatches(rot_bonds_smarts) 
+            imp_mol = DUMMY_MOL
+        torsions = []
+        for j, k in matches:
+            atom_j = imp_mol.GetAtomWithIdx(j)
+            atom_k = imp_mol.GetAtomWithIdx(k)
+            i = [n.GetIdx() for n in atom_j.GetNeighbors() if n.GetIdx() != k][0]
+            l = [n.GetIdx() for n in atom_k.GetNeighbors() if n.GetIdx() != j][0]
+            phi = rdMolTransforms.GetDihedralDeg(confs, i, j, k, l)
+            atoms = [imp_mol.GetAtomWithIdx(idx) for idx in (i, j, k, l)]
+            torsions.append({
+                "Atoms": tuple(a.GetSymbol() for a in atoms),
+                "Phi": round(phi, 2),
+            }) 
+        return {"Number of Rot. Bonds": len(matches), "Torsions Info": torsions}
 
-
-#|%%--%%| <G7fwsrep6J|Tk4Rq9bmeg>
+#|%%--%%| <G7fwsrep6J|tMQLbCQO6K>
+zed = BytesPDB(data_path) 
+inst = MoleculeSorter(zed)
+inst.pdb_mols
+#|%%--%%| <tMQLbCQO6K|Tk4Rq9bmeg>
 zed = BytesPDB(data_path) 
 inst = MoleculeSorter(zed)
 inst.analyze_all() 
+inst.mol_sorted_dict
 #|%%--%%| <Tk4Rq9bmeg|Ct5CBZ9ecE>
 zed = BytesPDB(data_path) 
 zed.bookeeper()
-zed.mol_pdb() 
-xyz = zed.smi_pdb_dict
-xyz.keys()
-xyz["PDB MOLS"]
+zed.mol_pdb()
+inst.analyze_all()
+#xyz = zed.smi_pdb_dict
+#xyz.keys()
+#xyz["PDB MOLS"]
 
+
+#                "Torsion Idxs": (i,
+#            print(f"Rot. Bond: ({j}-{k}), Torsion: ({i},{j},{k},{l}): {angle:.2f}°") 
